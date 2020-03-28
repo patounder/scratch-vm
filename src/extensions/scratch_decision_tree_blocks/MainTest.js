de9a1b509ae28cd176fc72d8d1f71411a3aa7994
Patricio Quiroz <pmquirozvargas@gmail.com>
2020-03-28 14:42:44 -0300

    remove unused logs

--- src/extensions/scratch_decision_tree_blocks/MainTest.js
+++ src/extensions/scratch_decision_tree_blocks/MainTest.js
@@ -1,23 +1,12 @@
-const DataRecord = require('./DataRecordClass');
-const TrainingDataset = require('./TrainingDatasetClass');
 const ID3 = require('./ID3Class');
-const log = require('../../util/log');
 
 class MainTest{
 
     static main (trainingDataset, recordToClassify){
         const id3Service = new ID3();
-
-        const weatherTrainingDataset = new TrainingDataset(trainingDataset.attributes, trainingDataset.records);
-        const rec = new DataRecord(recordToClassify.values.slice());
-
-        log.log(weatherTrainingDataset);
-        log.log(rec);
-
-        const tree = id3Service.treeGrowth(weatherTrainingDataset, weatherTrainingDataset.attributes, 'playtennis');
-        log.log(tree);
-
-        return id3Service.classifyRecord(rec, tree, weatherTrainingDataset.attributes);
+        const tree = id3Service.treeGrowth(trainingDataset, trainingDataset.attributes,
+            'playtennis', 'root');
+        return id3Service.classifyRecord(recordToClassify, tree, trainingDataset.attributes);
     }
 }
 
