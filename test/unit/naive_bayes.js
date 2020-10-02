47b95a6fd4c36b254b407428b9af91b5303d0612
patounder <pmquirozvargas@gmail.com>
2020-10-02 13:26:37 -0300

    finish unit test for initSchema function

--- test/unit/naive_bayes.js
+++ test/unit/naive_bayes.js
@@ -5,35 +5,43 @@ const FrequencyTable = require('../../src/extensions/scratch3_naive_bayes/freq_t
 
 test('initSchema', t => {
 
-    const naiveBayes = new NaiveBayes(new Schema())
-    const mainAttr = 'disfrutaJugar'
-    const arrayAttr = 'cielo,temperatura,humedad,viento'.toLowerCase().split(',')
+    const naiveBayes = new NaiveBayes(new Schema());
+    const mainAttr = 'disfrutaJugar';
+    const arrayAttr = 'cielo,temperatura,humedad,viento'.toLowerCase().split(',');
 
     naiveBayes.initSchema(mainAttr, arrayAttr);
 
-    t.equals(naiveBayes._schema.mainAttribute, 'disfrutaJugar')
-    t.equals(naiveBayes._schema.totalCountTraining, 0)
+    t.equals(naiveBayes._schema.mainAttribute, 'disfrutaJugar');
+    t.equals(naiveBayes._schema.totalCountTraining, 0);
     t.equals(naiveBayes._schema.frequencyTablesMap.size, 4);
 
-    const freqCieloTable = naiveBayes._schema.frequencyTablesMap.get('cielo')
-    t.type(freqCieloTable, FrequencyTable)
-    t.equals(freqCieloTable._tableName, 'cielo')
-    t.equals(freqCieloTable._recordIndex, 0)
+    const freqCieloTable = naiveBayes._schema.frequencyTablesMap.get('cielo');
+    t.type(freqCieloTable, FrequencyTable);
+    t.equals(freqCieloTable._tableName, 'cielo');
+    t.equals(freqCieloTable._recordIndex, 0);
+    t.equals(freqCieloTable._frequencyMap.size, 0);
+    t.equals(freqCieloTable._attributeValues.length, 0);
 
     const freqTempTable = naiveBayes._schema.frequencyTablesMap.get('temperatura');
-    t.type(freqTempTable, FrequencyTable)
-    t.equals(freqTempTable._tableName, 'temperatura')
-    t.equals(freqTempTable._recordIndex, 1)
+    t.type(freqTempTable, FrequencyTable);
+    t.equals(freqTempTable._tableName, 'temperatura');
+    t.equals(freqTempTable._recordIndex, 1);
+    t.equals(freqTempTable._frequencyMap.size, 0);
+    t.equals(freqTempTable._attributeValues.length, 0);
 
     const freqHumeTable = naiveBayes._schema.frequencyTablesMap.get('humedad');
-    t.type(freqHumeTable, FrequencyTable)
-    t.equals(freqHumeTable._tableName, 'humedad')
-    t.equals(freqHumeTable._recordIndex, 2)
+    t.type(freqHumeTable, FrequencyTable);
+    t.equals(freqHumeTable._tableName, 'humedad');
+    t.equals(freqHumeTable._recordIndex, 2);
+    t.equals(freqHumeTable._frequencyMap.size, 0);
+    t.equals(freqHumeTable._attributeValues.length, 0);
 
     const freqVientoTable = naiveBayes._schema.frequencyTablesMap.get('viento');
-    t.type(freqVientoTable, FrequencyTable)
-    t.equals(freqVientoTable._tableName, 'viento')
-    t.equals(freqVientoTable._recordIndex, 3)
+    t.type(freqVientoTable, FrequencyTable);
+    t.equals(freqVientoTable._tableName, 'viento');
+    t.equals(freqVientoTable._recordIndex, 3);
+    t.equals(freqVientoTable._frequencyMap.size, 0);
+    t.equals(freqVientoTable._attributeValues.length, 0);
 
     t.end();
 });
