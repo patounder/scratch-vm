0a81d0575cea2cac5b7a145d56cba1fa4708f0d8
patounder <pmquirozvargas@gmail.com>
2021-06-19 14:03:24 -0400

    define maxima test

---
+++ test/unit/naive_bayes/maxima_test.js
@@ -0,0 +1,26 @@
+const test = require('tap').test;
+const NaiveBayes = require('../../../src/extensions/scratch3_naive_bayes/naive-bayes');
+const Model = require('../../../src/extensions/scratch3_naive_bayes/model');
+const MockInputsNaiveBayes = require('./mock_inputs');
+
+test('maxima', maximaTest => {
+    var naiveBayes = new NaiveBayes();
+    naiveBayes.initModel('emocion');
+    naiveBayes.train('alegria', MockInputsNaiveBayes.getArrayHappyExamples(), MockInputsNaiveBayes.getLengthArrayHappy());
+    naiveBayes.train('tristeza', MockInputsNaiveBayes.getArraySadExamples(), MockInputsNaiveBayes.getLengthArraySad());
+
+    var newValue = 'eres malvado conmigo';
+    var happyResultTeoBayes = naiveBayes.teoBayes('alegria', newValue);
+    var sadResultTeoBayes = naiveBayes.teoBayes('tristeza', newValue);
+    var resultBayes = [happyResultTeoBayes, sadResultTeoBayes];
+    var selectedCategory = naiveBayes.maxCategoryFrom(resultBayes);
+    maximaTest.equals(selectedCategory, 'tristeza');
+
+    var otherValue = 'lo hiciste increible';
+    var otherHappyResultTeoBayes = naiveBayes.teoBayes('alegria', otherValue);
+    var otherSadResultTeoBayes = naiveBayes.teoBayes('tristeza', otherValue);
+    var otherResultBayes = [otherHappyResultTeoBayes, otherSadResultTeoBayes];
+    var otherSelectedCategory = naiveBayes.maxCategoryFrom(otherResultBayes);
+    maximaTest.equals(otherSelectedCategory, 'alegria');
+    maximaTest.end();
+});
