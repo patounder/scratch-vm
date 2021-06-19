be4165f2f20aaf728184267cae5586a7b021bcd6
patounder <pmquirozvargas@gmail.com>
2021-06-19 13:33:40 -0400

    improve structure for unit tests

--- test/unit/naive_bayes.js
+++ test/unit/naive_bayes.js
@@ -1,88 +1,11 @@
 const test = require('tap').test;
 const NaiveBayes = require('../../src/extensions/scratch3_naive_bayes/naive-bayes');
 const Model = require('../../src/extensions/scratch3_naive_bayes/model');
-const NaiveBayesInputsStub = require('./stub/naive_bayes_inputs')
+const NaiveBayesInputsStub = require('./naive_bayes/mock_inputs')
 
 test('clasiffy text', clasiffyTextTester => {
 
     const classifierName = 'emocion';
-
-    clasiffyTextTester.test('init model', initModelTester => {
-
-        initModelTester.test('classifier with undefined name', initUndefinedName => {
-            var naiveBayes = new NaiveBayes();
-            naiveBayes.initModel();
-            initUndefinedName.equals(naiveBayes.model, undefined);
-            initUndefinedName.end();
-        });
-
-        initModelTester.test('classifier with default name', initDefaultName => {
-            const CATEGORY_DEFAULT_VALUE = 'nombre';
-            var naiveBayes = new NaiveBayes();
-            naiveBayes.initModel(CATEGORY_DEFAULT_VALUE);
-
-            initDefaultName.equals(naiveBayes.model.name, 'nombre');
-
-            initDefaultName.type(naiveBayes.model.mapCounterCategoryExamples, Map);
-            initDefaultName.equals(naiveBayes.model.mapCounterCategoryExamples.size, 0);
-
-            initDefaultName.type(naiveBayes.model.mapBagWordsForCategory, Map);
-            initDefaultName.equals(naiveBayes.model.mapBagWordsForCategory.size, 0);
-
-            initDefaultName.equals(naiveBayes.model.counterTotalExamples, 0);
-
-            initDefaultName.equals(naiveBayes.model.arrayVocabulary.length, 0);
-
-            initDefaultName.type(naiveBayes.model.mapBayesResult, Map);
-            initDefaultName.equals(naiveBayes.model.mapBayesResult.size, 0);
-
-            initDefaultName.end();
-        });
-
-        initModelTester.test('classifier name string', initStringName =>{
-            var naiveBayes = new NaiveBayes();
-            naiveBayes.initModel(classifierName);
-            initStringName.equals(naiveBayes.model.name, 'emocion');
-
-            initStringName.type(naiveBayes.model.mapCounterCategoryExamples, Map);
-            initStringName.equals(naiveBayes.model.mapCounterCategoryExamples.size, 0);
-
-            initStringName.type(naiveBayes.model.mapBagWordsForCategory, Map);
-            initStringName.equals(naiveBayes.model.mapBagWordsForCategory.size, 0);
-
-            initStringName.equals(naiveBayes.model.counterTotalExamples, 0);
-
-            initStringName.equals(naiveBayes.model.arrayVocabulary.length, 0);
-
-            initStringName.type(naiveBayes.model.mapBayesResult, Map);
-            initStringName.equals(naiveBayes.model.mapBayesResult.size, 0);
-            initStringName.end();
-        });
-
-        initModelTester.test('classifier with number', initNumberName =>{
-            var naiveBayes = new NaiveBayes();
-            naiveBayes.initModel(1234);
-            initNumberName.equals(naiveBayes.model.name, 1234);
-
-            initNumberName.type(naiveBayes.model.mapCounterCategoryExamples, Map);
-            initNumberName.equals(naiveBayes.model.mapCounterCategoryExamples.size, 0);
-
-            initNumberName.type(naiveBayes.model.mapBagWordsForCategory, Map);
-            initNumberName.equals(naiveBayes.model.mapBagWordsForCategory.size, 0);
-
-            initNumberName.equals(naiveBayes.model.counterTotalExamples, 0);
-
-            initNumberName.equals(naiveBayes.model.arrayVocabulary.length, 0);
-
-            initNumberName.type(naiveBayes.model.mapBayesResult, Map);
-            initNumberName.equals(naiveBayes.model.mapBayesResult.size, 0);
-            initNumberName.end();
-        });
-
-
-
-        initModelTester.end();
-    });
 
     clasiffyTextTester.test('training model', trainTextTester => {
         var naiveBayes = new NaiveBayes();
@@ -184,15 +107,3 @@ test('clasiffy text', clasiffyTextTester => {
     clasiffyTextTester.end();
 });
 
-test('remove symbols', removeSymbolsTest =>{
-
-    var funRemoveSymbols = function (myStr) {
-        return myStr.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    };
-
-    removeSymbolsTest.equals(funRemoveSymbols('Alegría'), 'alegria');
-    removeSymbolsTest.equals(funRemoveSymbols('VideoJuegos'), 'videojuegos');
-    removeSymbolsTest.equals(funRemoveSymbols('Video Juegos'), 'video juegos');
-
-    removeSymbolsTest.end();
-});
---
+++ test/unit/naive_bayes/initModel_test.js
@@ -0,0 +1,93 @@
+const test = require('tap').test;
+const NaiveBayes = require('../../../src/extensions/scratch3_naive_bayes/naive-bayes');
+const Model = require('../../../src/extensions/scratch3_naive_bayes/model');
+
+
+test('remove symbols', removeSymbolsTest =>{
+
+    var funRemoveSymbols = function (myStr) {
+        return myStr.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    };
+
+    removeSymbolsTest.equals(funRemoveSymbols('Alegría'), 'alegria');
+    removeSymbolsTest.equals(funRemoveSymbols('VideoJuegos'), 'videojuegos');
+    removeSymbolsTest.equals(funRemoveSymbols('Video Juegos'), 'video juegos');
+
+    removeSymbolsTest.end();
+});
+
+
+test('init model', initModel =>{
+
+    initModel.test('classifier with undefined name', undefinedName => {
+        var naiveBayes = new NaiveBayes();
+        naiveBayes.initModel();
+        undefinedName.equals(naiveBayes.model, undefined);
+        undefinedName.end();
+    });
+
+    initModel.test('classifier with default name', defaultName => {
+        const CATEGORY_DEFAULT_VALUE = 'nombre';
+        var naiveBayes = new NaiveBayes();
+        naiveBayes.initModel(CATEGORY_DEFAULT_VALUE);
+
+        defaultName.equals(naiveBayes.model.name, 'nombre');
+
+        defaultName.type(naiveBayes.model.mapCounterCategoryExamples, Map);
+        defaultName.equals(naiveBayes.model.mapCounterCategoryExamples.size, 0);
+
+        defaultName.type(naiveBayes.model.mapBagWordsForCategory, Map);
+        defaultName.equals(naiveBayes.model.mapBagWordsForCategory.size, 0);
+
+        defaultName.equals(naiveBayes.model.counterTotalExamples, 0);
+
+        defaultName.equals(naiveBayes.model.arrayVocabulary.length, 0);
+
+        defaultName.type(naiveBayes.model.mapBayesResult, Map);
+        defaultName.equals(naiveBayes.model.mapBayesResult.size, 0);
+
+        defaultName.end();
+    });
+
+    initModel.test('classifier name string', stringName =>{
+        var naiveBayes = new NaiveBayes();
+        naiveBayes.initModel('emocion');
+        stringName.equals(naiveBayes.model.name, 'emocion');
+
+        stringName.type(naiveBayes.model.mapCounterCategoryExamples, Map);
+        stringName.equals(naiveBayes.model.mapCounterCategoryExamples.size, 0);
+
+        stringName.type(naiveBayes.model.mapBagWordsForCategory, Map);
+        stringName.equals(naiveBayes.model.mapBagWordsForCategory.size, 0);
+
+        stringName.equals(naiveBayes.model.counterTotalExamples, 0);
+
+        stringName.equals(naiveBayes.model.arrayVocabulary.length, 0);
+
+        stringName.type(naiveBayes.model.mapBayesResult, Map);
+        stringName.equals(naiveBayes.model.mapBayesResult.size, 0);
+        stringName.end();
+    });
+
+    initModel.test('classifier with number', numberName =>{
+        var naiveBayes = new NaiveBayes();
+        naiveBayes.initModel(1234);
+        numberName.equals(naiveBayes.model.name, 1234);
+
+        numberName.type(naiveBayes.model.mapCounterCategoryExamples, Map);
+        numberName.equals(naiveBayes.model.mapCounterCategoryExamples.size, 0);
+
+        numberName.type(naiveBayes.model.mapBagWordsForCategory, Map);
+        numberName.equals(naiveBayes.model.mapBagWordsForCategory.size, 0);
+
+        numberName.equals(naiveBayes.model.counterTotalExamples, 0);
+
+        numberName.equals(naiveBayes.model.arrayVocabulary.length, 0);
+
+        numberName.type(naiveBayes.model.mapBayesResult, Map);
+        numberName.equals(naiveBayes.model.mapBayesResult.size, 0);
+        numberName.end();
+    });
+
+    initModel.end();
+});
